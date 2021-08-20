72a2e938255a7f8fc42ea31af6cc6bd53ff89960
Oliver Cassagneau-Francis <ollie.francis@gmail.com>
2021-08-20 18:40:47 +0200

    Document the package itself

---
+++ R/dHM2013R-package.R
@@ -0,0 +1,12 @@
+# script to create package documentation
+
+#' dHM2013R: A package for implementing the estimation procedure in dH+M (2013).
+#'
+#' The package includes a main function \code{progDHM2013} that implements the
+#' procedure, and another function to estimate suitable data on which to test
+#' the estimation.
+#'
+#' @import data.table
+#' @importFrom magrittr %>%
+#' @keywords internal
+"_PACKAGE"
